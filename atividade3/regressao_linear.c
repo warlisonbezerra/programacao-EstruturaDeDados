8edc4b2505b42aabb6e079846e1742fec01c5e76
Your Name <you@example.com>
2023-08-28 00:18:36 -0300

    Adicionando a atividade 3, porém sem resolução

--- atividade3/regressao_linear.c
+++ atividade3/regressao_linear.c
@@ -1,64 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-typedef struct Ponto {
-    double x;
-    double y;
-} Ponto;
+#define MAX_LINE_LENGTH 100
 
-Ponto* lerPontos(char* nomeArquivo, int* numPontos) {
-    FILE* arquivo = fopen(nomeArquivo, "r");
-    if (arquivo == NULL) {
-        printf("Não foi possível abrir o arquivo.\n");
-        return NULL;
-    }
+int main() {
+    char filename[] = "dados.csv"; // Nome do arquivo CSV
+    FILE *file = fopen(filename, "r");
 
-    Ponto* pontos = NULL;
-    double x, y;
-    *numPontos = 0;
-
-    while (fscanf(arquivo, "%lf,%lf", &x, &y) == 2) {
-        pontos = realloc(pontos, (*numPontos + 1) * sizeof(Ponto));
-        pontos[*numPontos].x = x;
-        pontos[*numPontos].y = y;
-        (*numPontos)++;
-    }
-
-    fclose(arquivo);
-    return pontos;
-}
-
-void calcularRegressaoLinear(Ponto* pontos, int numPontos) {
-    double somaX = 0, somaY = 0, somaXY = 0, somaXQuadrado = 0;
-    for (int i = 0; i < numPontos; i++) {
-        somaX += pontos[i].x;
-        somaY += pontos[i].y;
-        somaXY += pontos[i].x * pontos[i].y;
-        somaXQuadrado += pontos[i].x * pontos[i].x;
-    }
-
-    double mediaX = somaX / numPontos;
-    double mediaY = somaY / numPontos;
-
-    double m = (numPontos * somaXY - somaX * somaY) / (numPontos * somaXQuadrado - somaX * somaX);
-    double b = mediaY - m * mediaX;
-
-    printf("A equação da linha de regressão linear é: y = %.2lf * x + %.2lf\n", m, b);
-}
-
-int main(int argc, char** argv) {
-    if (argc != 2) {
-        printf("Uso: %s <nome do arquivo>\n", argv[0]);
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
         return 1;
     }
 
-    int numPontos;
-    Ponto* pontos = lerPontos(argv[1], &numPontos);
-
-    if (pontos != NULL) {
-        calcularRegressaoLinear(pontos, numPontos);
-        free(pontos);
+    char line[MAX_LINE_LENGTH];
+    while (fgets(line, sizeof(line), file)) {
+        // Usamos a função strtok para separar os valores da linha usando vírgulas como delimitadores
+        char *token = strtok(line, ",");
+        while (token != NULL) {
+            printf("%s ", token);
+            token = strtok(NULL, ",");
+        }
+        printf("\n");
     }
+
+    fclose(file);
 
     return 0;
 }
